ce81d29b9624cd3e2850fb95629599e7321c9c22
Lukas Jacomin <lukasjacomin@gmail.com>
2018-02-20 14:40:05 -0300

    Test

--- src/background.js
+++ src/background.js
@@ -1,10 +1,3 @@
-var channels = [
-   "/channel/UCIR9VKPE70KJ4DX2zCjHObw",
-   "/channel/UC_gV70G_Y51LTa3qhu8KiEA",
-   "/user/felipeneto",
-   "/user/luccasneto"
-]
-
 let reg = /^.+.com/;
 
 chrome.tabs.onUpdated.addListener(function (tabId, data, tab) {
